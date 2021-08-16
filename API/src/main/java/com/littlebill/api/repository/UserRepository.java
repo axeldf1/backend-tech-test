c40fdc37f84c670f61bc5ca0b72ed9f72d74f34f
axeldf1 <47138823+axeldf1@users.noreply.github.com>
2021-08-16 02:23:23 +0200

    Delete

--- API/src/main/java/com/littlebill/api/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.littlebill.api.repository;
-
-import com.littlebill.api.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, String> {
-}
